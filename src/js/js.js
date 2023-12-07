58cd523d7f170f9c9cd800f064676d4844b80f23
ctepashka <125039221+ctepashka@users.noreply.github.com>
2023-12-07 21:38:42 +0300

    Add files via upload

---
+++ src/js/js.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        mousewhell: true,
+        keyboard: true,
+    });
+ 
